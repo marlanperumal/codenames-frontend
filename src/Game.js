5c9b5b95f196be4edba5d649a576382338de2545
Marlan Perumal <marlan.perumal@gmail.com>
2020-04-19 17:56:39 +0200

    added games with ids and home page

---
+++ src/Game.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react"
+import { useParams, useHistory, Link } from "react-router-dom"
+import axios from "axios"
+import styled from "styled-components"
+
+const teamColors = {
+    blue: "#1d3e6e",
+    red: "#900001",
+    neutral: "gray",
+    death: "black",
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    background-color: #b4b4b4;
+    height: 100%;
+`
+
+const Row = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+`
+
+const Card = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 4px;
+    padding: 8px;
+    text-transform: uppercase;
+    font-size: 1em;
+    font-family: inherit;
+    background-color: ${props =>
+        props.reveal ? teamColors[props.team] : "#EEEEEE"};
+    color: ${props => (props.reveal ? "white" : "auto")};
+    border-style: solid;
+    border-width: 4px;
+    border-color: white;
+    border-radius: 8px;
+    width: 120px;
+    height: 90px;
+    box-shadow: 4px 4px 3px grey;
+    /* outline: 0; */
+
+    &:hover {
+        border-color: ${props => (props.disabled ? "white" : "greenyellow")};
+        cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
+    }
+`
+
+const Button = styled.button`
+    margin: 4px;
+    font-family: inherit;
+    width: 120px;
+    height: 50px;
+    background-color: white;
+`
+
+function arrayToObject(arr, idKey) {
+    return arr.reduce((obj, item) => ({ ...obj, [item[idKey]]: item }), {})
+}
+
+function Game() {
+    const [cards, setCards] = useState({})
+    const [cardGrid, setCardGrid] = useState([])
+    const [refresh, setRefresh] = useState(false)
+    const [revealBoard, setRevealBoard] = useState(false)
+
+    const { gameId } = useParams()
+    const history = useHistory()
+
+    
+    useEffect(() => {
+        async function setupBoard() {
+            const response = await axios.get(`/api/cards?game_id=${gameId}`)
+            const getCardsResponse = response.data
+            const newCards = arrayToObject(getCardsResponse, "id")
+            const newCardGrid = []
+            const cardsPerRow = 5
+            for (let i = 0; i < getCardsResponse.length / cardsPerRow; i++) {
+                const start = i * cardsPerRow
+                newCardGrid.push(
+                    getCardsResponse
+                        .slice(start, start + cardsPerRow)
+                        .map(item => item.id),
+                )
+            }
+            setCards(newCards)
+            setCardGrid(newCardGrid)
+        }
+        setupBoard()
+    }, [refresh, gameId])
+
+    async function newGame() {
+        const response = await axios.post("/api/games")
+        const game = response.data
+        setCardGrid([])
+        history.push(`/${game.id}`)
+    }
+
+    async function selectCard(cardId) {
+        const card = cards[cardId]
+        await axios.patch(`/api/cards/${cardId}`, { selected: !card.selected })
+        setCards({
+            ...cards,
+            [cardId]: {
+                ...card,
+                selected: !card.selected,
+            },
+        })
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Link to="/">
+                    <Button>Home</Button>
+                </Link>
+                <Button onClick={() => setRevealBoard(!revealBoard)}>
+                    {revealBoard ? "Conceal" : "Reveal"}
+                </Button>
+                <Button onClick={() => setRefresh(!refresh)}>Refresh</Button>
+                <Button onClick={() => newGame()}>New Game</Button>
+            </Row>
+            {cardGrid.map((row, rowId) => (
+                <Row key={rowId}>
+                    {row.map(cardId => {
+                        const card = cards[cardId]
+                        return (
+                            <Card
+                                key={cardId}
+                                team={card.team}
+                                reveal={revealBoard || card.selected}
+                                disabled={revealBoard}
+                                onClick={() => selectCard(cardId)}
+                            >
+                                {(revealBoard || !card.selected) && card.word}
+                            </Card>
+                        )
+                    })}
+                </Row>
+            ))}
+        </Container>
+    )
+}
+
+export default Game
