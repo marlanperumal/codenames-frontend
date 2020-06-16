3cdc7383657e070896a97b99dc4e7ae3bc092395
Marlan Perumal <marlan.perumal@gmail.com>
2020-06-16 14:04:29 +0200

    minor adjustments to the gradients

--- src/Room.js
+++ src/Room.js
@@ -9,9 +9,9 @@ const socket = io()
 
 const teamColors = {
     BLUE: "linear-gradient(45deg, #1d2e6e, #1d4e8e)",
-    RED: "linear-gradient(45deg, #700, #A00)",
-    NEUTRAL: "linear-gradient(45deg, #444, #666)",
-    DEATH: "linear-gradient(45deg, #000, #333)",
+    RED: "linear-gradient(45deg, #700, #b00)",
+    NEUTRAL: "linear-gradient(45deg, #444, #777)",
+    DEATH: "linear-gradient(45deg, #000, #222)",
 }
 
 const Body = styled.div`
